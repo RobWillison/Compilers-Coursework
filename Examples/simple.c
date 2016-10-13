08e4e88e329450cf24eb08e4c1372c02703f7e75
RobWillison <Rob@Willison.co.uk>
2016-10-13 12:46:28 +0100

    added arithmatic stuff to TAC

--- Examples/simple.c
+++ Examples/simple.c
@@ -1,4 +1,4 @@
 int main()
 {
-    return 5;
+  return 8 + 2 * 3;
 }
--- compiler.c
+++ compiler.c
@@ -13,6 +13,12 @@ int current_reg = 0;
 
 extern TAC *compile(NODE *tree);
 
+void add_TAC_to_list(TAC *front, TAC *tail)
+{
+  while (front->next != 0) front = front->next;
+  front->next = tail;
+}
+
 char *get_location(LOCATION *loc)
 {
   if (loc->type == LOCREG)
@@ -34,15 +40,24 @@ char *get_location(LOCATION *loc)
 
 void print_tac(TAC *tac_code)
 {
-  while (tac_code != 0) {
-    if (tac_code->operation = 'S') {
-      LOCATION *destination = tac_code->destination;
-      LOCATION *operand_one = tac_code->operand_one;
+  if (tac_code == 0) return;
 
-      printf("%s := %s\n", get_location(destination), get_location(operand_one));
-    }
-    tac_code = tac_code->next;
+  print_tac(tac_code->next);
+
+  if (tac_code->operation == 'S') {
+    LOCATION *destination = tac_code->destination;
+    LOCATION *operand_one = tac_code->operand_one;
+
+    printf("%s := %s\n", get_location(destination), get_location(operand_one));
+  } else {
+    LOCATION *destination = tac_code->destination;
+    LOCATION *operand_one = tac_code->operand_one;
+    LOCATION *operand_two = tac_code->operand_two;
+
+    printf("%s := %s %s %s\n", get_location(destination), get_location(operand_one), named(tac_code->operation), get_location(operand_two));
   }
+
+
 
 }
 
@@ -69,30 +84,69 @@ LOCATION *next_reg()
   return loc;
 }
 
+TAC *compile_math(NODE *tree)
+{
+  TAC *operand_one = compile(tree->left);
+  TAC *operand_two = compile(tree->right);
+
+  TAC *operation = new_tac(next_reg());
+  operation->operation = tree->type;
+  operation->operand_one = operand_one->destination;
+  operation->operand_two = operand_two->destination;
+
+  add_TAC_to_list(operand_two, operand_one);
+  add_TAC_to_list(operation, operand_two);
+
+  return operation;
+}
+
+TAC *compile_return(NODE *tree)
+{
+  TAC *operand_tac = compile(tree->left);
+
+  TAC *return_tac = new_tac(next_reg());
+  return_tac->operation = 'S';
+  return_tac->operand_one = operand_tac->destination;
+  return_tac->next = operand_tac;
+
+  return return_tac;
+}
+
+TAC *compile_leaf(NODE *tree)
+{
+  if(tree->left->type == CONSTANT) {
+    TOKEN *t = (TOKEN *)tree->left;
+    LOCATION *loc = new_location(LOCTOKEN);
+    loc->token = t;
+
+    TAC *taccode = new_tac(next_reg());
+    taccode->operation = 'S';
+    taccode->operand_one = loc;
+
+    return taccode;
+  }
+}
+
 TAC *compile(NODE *tree)
 {
   printf("NEXT TREE\n");
   print_tree(tree);
 
-  if (tree->type == RETURN)
-  {
-    TAC *operand_tac = compile(tree->left);
-    operand_tac->next = new_tac(next_reg());
-    operand_tac->next->operation = 'S';
-    operand_tac->next->operand_one = operand_tac->destination;
-
-    return operand_tac;
-  } else if (tree->type == LEAF) {
-      if(tree->left->type == CONSTANT) {
-        TOKEN *t = (TOKEN *)tree->left;
-        LOCATION *loc = new_location(LOCTOKEN);
-        loc->token = t;
-
-        TAC *taccode = new_tac(next_reg());
-        taccode->operation = 'S';
-        taccode->operand_one = loc;
-
-        return taccode;
-      }
+  switch (tree->type) {
+    case RETURN:
+      return compile_return(tree);
+    case LEAF:
+      return compile_leaf(tree);
+    case '<':
+    case '>':
+    case '*':
+    case '/':
+    case '+':
+    case '-':
+    case LE_OP:
+    case GE_OP:
+    case EQ_OP:
+    case NE_OP:
+      return compile_math(tree);
   }
 }
