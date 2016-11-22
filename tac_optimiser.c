19fa3f6a8dba8774cc308335a8bbc59b6e0a7de9
RobWillison <Rob@willison.co.uk>
2016-11-22 13:57:35 +0000

    started next use info stuf

--- tac_optimiser.c
+++ tac_optimiser.c
@@ -7,9 +7,98 @@
 #include "tacBlock.h"
 #include "definitions.h"
 
+typedef struct NEXT_USE_INFO
+{
+  struct VARIABLE_NEXT_USE *variable;
+  struct NEXT_USE_INFO *next;
+} NEXT_USE_INFO;
+
+typedef struct VARIABLE_NEXT_USE
+{
+  struct LOCATION *location;
+  struct TAC *nextUse;
+  int live;
+  struct VARIABLE_NEXT_USE *next;
+} VARIABLE_NEXT_USE;
+
+NEXT_USE_INFO *nextUseInfoList = NULL;
+
+NEXT_USE_INFO *newNextUseInfo(VARIABLE_NEXT_USE *variable)
+{
+  NEXT_USE_INFO *nextUse = (NEXT_USE_INFO*)malloc(sizeof(NEXT_USE_INFO));
+  nextUse->variable = variable;
+
+  return nextUse;
+}
+
+void addToEndOfNextUseList(VARIABLE_NEXT_USE *variable, VARIABLE_NEXT_USE *list)
+{
+  VARIABLE_NEXT_USE *pointer = list;
+  while (pointer->next) pointer = pointer->next;
+  pointer->next = variable;
+}
+
+VARIABLE_NEXT_USE *addNextUseInfo(LOCATION *location, int live, TAC *nextUse)
+{
+  VARIABLE_NEXT_USE *variable = (VARIABLE_NEXT_USE*)malloc(sizeof(VARIABLE_NEXT_USE));
+  variable->live = live;
+  variable->nextUse = nextUse;
+
+  if (!nextUseInfoList)
+  {
+    nextUseInfoList = newNextUseInfo(variable);
+    return;
+  }
+
+  NEXT_USE_INFO *pointer = nextUseInfoList;
+  while(pointer)
+  {
+    if (((VARIABLE_NEXT_USE*)pointer->variable)->location == location)
+    {
+      addToEndOfNextUseList(variable, pointer->variable);
+      return;
+    }
+
+    if (!pointer->next) break;
+
+    pointer = pointer->next;
+  }
+
+  pointer->next = newNextUseInfo(variable);
+}
+
+NEXT_USE_INFO *getNextUseInfoTac(TAC *tac)
+{
+  if (tac->next) getNextUseInfoTac(tac->next);
+
+  LOCATION *destination = tac->destination;
+  LOCATION *operandOne = tac->operand_one;
+  LOCATION *operandTwo = tac->operand_two;
+
+  if (destination)
+  {
+    addNextUseInfo(destination, 0, NULL);
+  }
+  if (operandOne)
+  {
+    addNextUseInfo(operandOne, 1, tac);
+  }
+  if (operandTwo)
+  {
+    addNextUseInfo(operandTwo, 1, tac);
+  }
+
+  return nextUseInfoList;
+}
+
+NEXT_USE_INFO *getNextUseInfo(TAC_BLOCK *input)
+{
+  return getNextUseInfoTac(input->tac);
+}
+
 void optimiseTacBlock(TAC_BLOCK *input)
 {
-  //constantFolding(input);
+  getNextUseInfo(input);
 }
 
 TAC_BLOCK *optimiseTac(TAC_BLOCK *input)
